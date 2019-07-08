c356a17d1da598883df7dcc36835d9cb39b69fe9
Orie Chinedu <oriechinedu@gmail.com>
2019-07-08 13:02:43 +0100

    Implement delete user

--- controllers/index.js
+++ controllers/index.js
@@ -63,8 +63,43 @@ function findUser(req, res) {
       });
     });
 }
+function deleteUser(req, res) {
+  const { id } = req.params
+  db.findById(id)
+    .then(data => {
+      if (data) {
+        db.remove(id)
+        .then(data => {
+          if (data) {
+            res.status(200).json({
+              status: "success",
+              message: 'User deleted successfully'
+            });
+          } else {
+            res.status(500).json({
+              status: "error",
+              errorMessage: `An error occurred trying to delete the user with id ${id}` 
+            });
+          }
+        })
+      }
+      else {
+        res.status(404).json({
+          status: "error",
+          message: "The user with the specified ID does not exist." 
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        status: "error",
+        error: "The user information could not be retrieved." 
+      });
+    });
+}
 module.exports = {
   getUsers,
   createUser,
-  findUser
+  findUser,
+  deleteUser
 };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 
-const { getUsers, createUser, findUser } = require('../controllers')
+const { getUsers, createUser, findUser, deleteUser } = require('../controllers')
 const router = express.Router()
 
 router.get('/users', getUsers)
 router.post('/users', createUser)
 router.get('/users/:id', findUser)
+router.delete('/users/:id', deleteUser)
 
 module.exports = {
   router: router
