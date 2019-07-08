ea763d4d7be277dd0f6983a2bf2fb5be255d5200
Orie Chinedu <oriechinedu@gmail.com>
2019-07-08 12:31:32 +0100

    Create routes file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const server = express()
+const { router } = require('./routes')
 
 server.use(express.json());
+server.use('/api', router)
 server.get('/', (req, res) => {
   return res.status(200).json({message: 'The server is running'})
 })
