3fe85350a686461328e2cd6aea88904191971017
Orie Chinedu <oriechinedu@gmail.com>
2019-07-08 12:20:17 +0100

    Setup server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// implement your API here
+const express = require('express')
+const server = express()
+
+server.use(express.json());
+server.get('/', (req, res) => {
+  return res.status(200).json({message: 'The server is running'})
+})
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`server listening to port ${PORT}`)
+})
